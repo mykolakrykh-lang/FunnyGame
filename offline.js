81bee222c8620ee4211a81fdf53631f9f56997b0
mykolakrykh-lang <mykolakrykh@gmail.com>
2025-12-01 21:39:23 +0200

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,28 @@
+﻿{
+	"version": 1764617222,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/cat-sheet0.png",
+		"images/tiledbackground.png",
+		"images/wall.png",
+		"images/tiledbackground2.png",
+		"images/tiledbackground3.png",
+		"images/sprite2-sheet0.png",
+		"images/sprite3-sheet0.png",
+		"images/r-sheet0.png",
+		"images/hear-sheet0.png",
+		"media/mtemtem.m4a",
+		"media/mtemtem.ogg",
+		"media/videoplayback-_1_.m4a",
+		"media/videoplayback-_1_.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
